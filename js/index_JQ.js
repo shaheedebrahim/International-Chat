f6bbcacece1ead848eceee3fbeebf1edbd7e675e
Numan <ahmet.aral@ucalgary.ca>
2018-04-07 11:00:03 -0600

    Account info check + updates and fixes

--- js/index_JQ.js
+++ js/index_JQ.js
@@ -1,6 +1,7 @@
 	// ========= //
 	// Variables //
-	// =======================================================
+	// ========= // ===========================================
+	
 	// Screens / Containers
 	var initialLogin = $('#initialLogin');
 	var loginScreen = $('#loginScreen');
@@ -24,13 +25,28 @@
 	var pw1 = $('#pw1');
 	var pw2= $('#pw2');
 	var pwReqs = [0, 0, 0, 0, 0];
+	var createAccount_Form = $('#createAccount_Form');
+	
 
-	// =======================================================
+	// ========================================================
     
+	
+	// =========== //
+	// Initial Run //
+	// =========== // =========================================
+	
+	$(function (){
+        		
+		// Hide all pages except home
+		hideAll();
+			
+		
+    });
 	
 	// ========= //
 	// Functions //
-	// =======================================================
+	// ========= // ===========================================
+	
 	// Got it button-click
 	gotIt_Button.click(function(e){
 		e.preventDefault();
@@ -64,29 +80,43 @@
 	// Next_Signup CreateAccount button-click
 	next_SignUp.click(function(e){
 		
+		// check if all the forms are filled
 		var empty = $(this).parent().find("input").filter(function() {
             return this.value === "";
         });
 		
+		// Check if all the pw req conditions are met
 		var pwReqCompleted = JSON.stringify(pwReqs) === '[1,1,1,1,1]';
-		var pwMatch = pw1.value === pw2.value;
 		
-		if (pw1.value != pw2.value) {
-			pw2[0].setCustomValidity('Passwords must match.');
-			e.preventDefault();
-		} else {
-			pw2[0].setCustomValidity('');
-		}
-		
-		console.log(pwReqCompleted);
-		if(!empty.length && pwReqCompleted && pwMatch) {
-			e.preventDefault();
+		// continue to Next if it's a valid user info
+		if(!empty.length && pwReqCompleted) {			
 			createAccount_SignUp.hide();
 			createAccount_Language.show();		
+		}
+	});
+	
+	// NO LONGER NEEDED 
+	///////////////////////////////////
+	/* function accountRequirementsComplete(){
+		
+		var empty = $(next_SignUp).parent().find("input").filter(function() {
+            return next_SignUp.value === "";
+        });
+		
+		var pwReqCompleted = JSON.stringify(pwReqs) === '[1,1,1,1,1]';
+		// var pwMatch = pw1.value === pw2.value;
+		
+		if(!empty.length && pwReqCompleted) {
+			return true;	
 		} else {
 			return false;
 		}
-	});
+	}	 */
+	///////////////////////////////////
+	
+	
+	
+	
 	
 	// Next_Language CreateAccount button-click
 	next_Language.click(function(e){
@@ -130,99 +160,94 @@
 		e.preventDefault();
 		createAccount_Profile.hide();
 		createAccount_Language.show();		
-	});
+	});	
+
+	// ========================================================
 	
 	
-
-	// =======================================================
-        /* PW Requirements */
-
-        $('input[type=password]').keyup(function() {
-            var pswd = $(this).val();
-
-            //validate the length
-            if ( pswd.length >= 8 ) {
-				pwReqs[0] = 1;
-                $('#length').removeClass('invalid').addClass('valid');
-            } else {
-				pwReqs[0] = 0;
-				$('#length').removeClass('valid').addClass('invalid');                
-            }
-
-            //validate letter
-            if ( pswd.match(/[A-z]/) ) {
-				pwReqs[1] = 1;
-                $('#letter').removeClass('invalid').addClass('valid');
-            } else {
-				pwReqs[1] = 0;
-                $('#letter').removeClass('valid').addClass('invalid');
-            }
-
-            //validate capital letter
-            if ( pswd.match(/[A-Z]/) ) {
-				pwReqs[2] = 1;
-                $('#capital').removeClass('invalid').addClass('valid');
-            } else {
-				pwReqs[2] = 0;
-                $('#capital').removeClass('valid').addClass('invalid');
-            }
-
-            //validate number
-            if ( pswd.match(/\d/) ) {
-				pwReqs[3] = 1;
-                $('#number').removeClass('invalid').addClass('valid');
-            } else {
-				pwReqs[3] = 0;
-                $('#number').removeClass('valid').addClass('invalid');
-            }
-
-            //validate space
-            if ( pswd.match(/[^a-zA-Z0-9\-\/]/) ) {
-				pwReqs[4] = 1;
-                $('#space').removeClass('invalid').addClass('valid');
-            } else {
-				pwReqs[4] = 0;
-                $('#space').removeClass('valid').addClass('invalid');
-            }
-
-        }).focus(function() {
-            //placeReq(); //$('#pswd_info').show();
-			pwRequirements.show();
-        }).blur(function() {
-            $('#pswd_info').hide();
-			if(close.data('clicked')) {
-				close.click(function(e){ 
-					$(this).data('clicked', true);
-					$('#loginScreen').css("filter", "none");
-					createAccount_SignUp.hide();
-				});
-			}
-				
-        });
 	
-	// initial-run function
-	$(function (){
-        		
-		// Hide all pages except home
-		hideAll();
-			
-		
-    });
-
-
-
-    
+	
+    // Bring out the home screen
     function hideAll(){
 		loginScreen.hide();
 		createAccount_SignUp.hide();
 		createAccount_Language.hide();
 		createAccount_Profile.hide();
 	};
+	
+
+	/* PW Requirements */
+	// While typing and on focus (click)
+	// Avoids wrong solely on click as well
+	$('#pw1, #pw2').keyup(function(){
+		validatePassword(this);
+	}).focus(function() {
+		//placeReq(); //$('#pswd_info').show();
+		pwRequirements.show();
+		validatePassword(this);
+	}).blur(function() {
+		$('#pswd_info').hide();
+		if(close.data('clicked')) {
+			close.click(function(e){ 
+				$(this).data('clicked', true);
+				$('#loginScreen').css("filter", "none");
+				createAccount_SignUp.hide();
+			});
+		}
+			
+	});
 
 
     // Get PW Reqs
+	function validatePassword(pwBox){
+		var pswd = $(pwBox).val();
+		
+		//validate the length
+		if ( pswd.length >= 8 ) {
+			pwReqs[0] = 1;
+			$('#length').removeClass('invalid').addClass('valid');
+		} else {
+			pwReqs[0] = 0;
+			$('#length').removeClass('valid').addClass('invalid');                
+		}
 
+		//validate letter
+		if ( pswd.match(/[A-z]/) ) {
+			pwReqs[1] = 1;
+			$('#letter').removeClass('invalid').addClass('valid');
+		} else {
+			pwReqs[1] = 0;
+			$('#letter').removeClass('valid').addClass('invalid');
+		}
 
+		//validate capital letter
+		if ( pswd.match(/[A-Z]/) ) {
+			pwReqs[2] = 1;
+			$('#capital').removeClass('invalid').addClass('valid');
+		} else {
+			pwReqs[2] = 0;
+			$('#capital').removeClass('valid').addClass('invalid');
+		}
+
+		//validate number
+		if ( pswd.match(/\d/) ) {
+			pwReqs[3] = 1;
+			$('#number').removeClass('invalid').addClass('valid');
+		} else {
+			pwReqs[3] = 0;
+			$('#number').removeClass('valid').addClass('invalid');
+		}
+
+		//validate space
+		if ( pswd.match(/[^a-zA-Z0-9\-\/]/) ) {
+			pwReqs[4] = 1;
+			$('#space').removeClass('invalid').addClass('valid');
+		} else {
+			pwReqs[4] = 0;
+			$('#space').removeClass('valid').addClass('invalid');
+		}
+	}
+	
     function placeReq() {
         var position = pw1.position();
         var position2 = pw2.position();
@@ -237,3 +262,91 @@
         // pwRequirements.style.display = "inline";
         pwRequirements.show();
     };
+	
+	// $.validator.setDefaults( {
+			// submitHandler: function () {
+				// if (accountRequirementsComplete() === true){
+					// createAccount_SignUp.hide();
+					// createAccount_Language.show();		
+				// }
+			// }
+	// });
+	
+	
+	// Just in case:
+	/* submitHandler: function(form) {
+			// def action
+		}, */
+	
+	createAccount_Form.validate( {		
+		submitHandler: function(form) {
+			createAccount_SignUp.hide();
+			createAccount_Language.show();
+		},
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			username: {
+				required: true,
+				minlength: 7
+			},
+			pw1: {
+				required: true,
+				minlength: 8
+			},
+			pw2: {
+				required: true,
+				minlength: 8,
+				equalTo: "#pw1"
+			}
+		},
+		messages: {
+			email: "Please enter a valid email address",
+			username: {
+				required: "Please enter a username",
+				minlength: "Your username must consist of at least 7 characters long"
+			},
+			pw1: {
+				required: "Please provide a password",
+				minlength: "Your password must be at least 8 characters long --Will add more rules --"
+			},
+			pw2: {
+				required: "Please provide a password",
+				equalTo: "Please enter the same password as above"
+			}
+		},
+		errorElement: "em",
+		errorPlacement: function ( error, element ) {
+			// Add the `help-block` class to the error element
+			error.addClass( "help-block" );
+
+			// Add `has-feedback` class to the parent div.form-group
+			// in order to add icons to inputs
+			element.parents( ".form-group").addClass( "has-feedback" );
+
+			// error.insertAfter( element.parent( "label" ) );
+			error.insertAfter( element.parent("div") );
+			
+
+			// Add the span element, if doesn't exists, and apply the icon classes to it.
+			if ( !element.next( "span" )[ 0 ] ) {
+				$( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+			}
+		},
+		success: function ( label, element ) {
+			// Add the span element, if doesn't exists, and apply the icon classes to it.
+			if ( !$( element ).next( "span" )[ 0 ] ) {
+				$( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+			}
+		},
+		highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
+			$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+		},
+		unhighlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
+			$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+		}
+	});
