6a8b3ca4e9f69f4208ba610a8f8e74c8b5d12cfb
Shaheed Ebrahim <shaheed_ebrahim@hotmail.com>
2018-04-15 19:01:42 -0600

    Removed sillyname

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var generateName = require('sillyname');
 
 let chatHistory = {};
 
